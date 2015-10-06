9de974fde97502a3563af0f4ce9873c398e9fb6c
Ilya Semenov <semenov@iteratia.com>
2015-10-07 02:37:04 +0600

    Simplified urlpattern for robots.txt and friends

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,6 +1,3 @@
-import pathlib
-import re
-
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
@@ -9,11 +6,9 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-# robots.txt and friends
-root_files = [p.name for p in pathlib.Path(settings.STATICFILES_ROOT).iterdir() if p.is_file()]
-
 urlpatterns = [
-	url(r'^(%s)$' % '|'.join(map(re.escape, root_files)), 'django.views.static.serve', {'document_root': settings.STATICFILES_ROOT}),
+	# robots.txt and friends
+	url(r'^([^/]+)$', 'django.views.static.serve', {'document_root': settings.STATICFILES_ROOT}),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'', include('project.app.urls')),
 ]
