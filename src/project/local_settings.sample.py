eecb665ac2006f7e3bc0cfbf77a371476964ab10
Ilya Semenov <semenov@iteratia.com>
2016-09-01 20:43:31 +0700

    Reformatted settings.py, use environment settings if provided

--- src/project/local_settings.sample.py
+++ src/project/local_settings.sample.py
@@ -1,17 +1,3 @@
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': 'project',
-	},
-}
-
 DEBUG = True
 
-# RAVEN_CONFIG = {
-# 	'dsn': '-paste-dsn-here-',
-# }
-
-# INSTALLED_APPS = INSTALLED_APPS + (
-# 	'raven.contrib.django.raven_compat',
-# 	'django_maven',
-# )
+SECRET_KEY = 'not secret'
--- src/project/settings.py
+++ src/project/settings.py
@@ -1,11 +1,11 @@
 import os
 
+from environ import Env as DjangoEnv
+from environs import Env
+
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
-# Application definition
-
-INSTALLED_APPS = (
+INSTALLED_APPS = [
 	# Rules for this list:
 	# - Group apps by module
 	# - Sort groups by time (recently added go last)
@@ -17,10 +17,15 @@ INSTALLED_APPS = (
 	'django.contrib.sessions',
 	'django.contrib.staticfiles',
 	'project.app.apps.AppConfig',
-)
+]
 
-MIDDLEWARE_CLASSES = (
-	# Same rules as above, mostly. Django middleware go in default order.
+MIDDLEWARE_CLASSES = [
+	# Rules for this list:
+	# - Django middleware go in default order.
+	# - Group apps by module
+	# - Sort groups by time (recently added go last)
+	# - Sort apps within a group alphabetically
+	# - If you need to place an app out of order, leave a comment explaining the reason why.
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
@@ -29,43 +34,20 @@ MIDDLEWARE_CLASSES = (
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'django.middleware.security.SecurityMiddleware',
-)
+]
 
 ROOT_URLCONF = 'project.urls'
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
-
+USE_I18N = True
+USE_L10N = True
 LANGUAGE_CODE = 'en-us'
 
+USE_TZ = True
 TIME_ZONE = 'America/New_York'
 
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Security
-
-SECRET_KEY = '-change me-'  # generate a random key with 'pwgen -s 50 -n 1'
-
-
-# Templates
-
 TEMPLATES = [
-	{
-		'BACKEND': 'django.template.backends.jinja2.Jinja2',
-		'DIRS': [os.path.join(PROJECT_DIR, 'jinja2')],
-		'APP_DIRS': True,
-		'OPTIONS': {
-			'environment': 'project.jinja2.environment',
-		}
-	},
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
 		'DIRS': [os.path.join(PROJECT_DIR, 'templates')],
@@ -78,37 +60,49 @@ TEMPLATES = [
 	},
 ]
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'var', 'static')
 STATICFILES_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATICFILES_DIRS = (STATICFILES_ROOT,)
+STATICFILES_DIRS = [STATICFILES_ROOT]
 
-
-# Media files
-
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'var', 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'var', 'media')
 
-
-# Auth
+# AUTH_USER_MODEL = 'accounts.User'
 
 LOGIN_URL = '/login/'
-LOGOUT_URL = '/logout/'
 LOGIN_REDIRECT_URL = '/'
-
-
-# Mail
-
-SERVER_EMAIL = 'server@project.com'
-EMAIL_SUBJECT_PREFIX = '[project.com] '  # for mailing admins and managers
+LOGOUT_URL = '/logout/'
+LOGOUT_REDIRECT_URL = '/'
 
 DEFAULT_FROM_EMAIL = 'noreply@project.com'
 
+SERVER_EMAIL = 'server@project.com'
+EMAIL_SUBJECT_PREFIX = ''
 
-# Local
+#
+# Environment settings
+#
+
+env = Env()
+env.add_parser('db_settings', DjangoEnv.db_url_config)
+env.add_parser('email_settings', DjangoEnv.email_url_config)
+
+SECRET_KEY = env('SECRET_KEY', None)  # generate a random key with 'pwgen -s 50 -n 1'
+
+DEBUG = env.bool('DEBUG', False)
+SENTRY_DSN = env('SENTRY_DSN', None)
+if SENTRY_DSN:
+	RAVEN_CONFIG = {'dsn': SENTRY_DSN}
+	INSTALLED_APPS.insert(0, 'raven.contrib.django.raven_compat')
+
+DATABASES = {
+	'default': env.db_settings('DATABASE_URL', 'postgres://localhost/project')
+}
+
+if env('EMAIL_URL', None):
+	vars().update(env.email_settings('EMAIL_URL'))
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', '')
 
 from .local_settings import *
