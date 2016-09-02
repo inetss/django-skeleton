642c5c9db4b7d4b71193ca6cf122c4446b5a0fa3
Ilya Semenov <semenov@iteratia.com>
2016-09-02 15:14:27 +0700

    Reduced amount of copy/paste in docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-for d in var/static var/media; do
+for v in static media; do
+	d=var/$v
 	mkdir -p $d
 	find $d -type d -not -perm 755 -exec chmod 755 {} \; -exec chmod -s {} \;
 	find $d -type f -not -perm 644 -exec chmod 644 {} \;
 	chown -R app:nogroup $d
 done
 
-if [ ! -e src/project/local_settings.py ]; then
-	ln -s ${DJANGO_LOCAL_SETTINGS:-docker_settings.py} src/project/local_settings.py
+s=src/project/local_settings.py
+if [ ! -e "$s" ]; then
+	ln -sf "${DJANGO_LOCAL_SETTINGS:-docker_settings.py}" "$s"
 fi
 
 sudo -u app -E src/manage.py migrate --noinput
